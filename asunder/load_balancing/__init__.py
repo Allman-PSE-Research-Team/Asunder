e9b5e0d63e796633e37fa0908a57b6299c7836b4
Fortune Adekogbe <fortuneadekogbe@gmail.com>
2026-06-03 02:44:22 -0400

    Started accessing LoadBalancer from its source file directly.

--- asunder/load_balancing/__init__.py
+++ asunder/load_balancing/__init__.py
@@ -1,26 +1,6 @@
 """Load-balancing specific modules in Asunder."""
 
-def LoadBalancer(*args, **kwargs):
-    """
-    Run benchmark evaluations using :mod:`asunder.load_balancing.column_generation.LB`.
-    
-    Parameters
-    ----------
-    *args : Any
-        Additional positional arguments.
-    **kwargs : Any
-        Additional keyword arguments.
-    
-    Returns
-    -------
-    Any
-        Computed result.
-    """
-    from asunder.load_balancing.column_generation.LB import LoadBalancer as _LoadBalancer
-
-    return _LoadBalancer(*args, **kwargs)
-
-
+from asunder.load_balancing.column_generation.LB import LoadBalancer
 
 __all__ = [
     "algorithms",
